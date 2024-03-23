61811d1f5d9c0845ed1af1174224c5fa5aa011de
Nguyen Minh Tuan <tnowad@gmail.com>
2024-03-23 23:52:45 +0700

    chore: remove relationship between user and article
    feat : adding missing pages

---
+++ news-copilot-frontend/src/routes/(app)/(default)/License/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">import { Section} from "flowbite-svelte-blocks";
+import {FooterCopyright} from 'flowbite-svelte';
+</script>
+
+<Section name="testimonial">
+      <h1 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">Licensing Information</h1>
+      <p class="mb-1 font-light text-gray-500 lg:mb-12 sm:text-xl dark:text-gray-400">This website provides news content and utilizes AI-powered autocorrect technology to enhance accuracy and clarity.</p>
+     <h1 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">Content Licensing</h1>
+     <p> News Content: Licensed under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener noreferrer">Creative Commons Attribution (CC BY)</a>.</p>
+     <p class="lg:mb-12"> AI-Generated AutoCorrecter : Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">Creative Commons Attribution-NonCommercial-ShareAlike (CC BY-NC-SA)</a>.</p>
+     <h1 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">Copyright</h1>
+     <FooterCopyright href="/" by="News Copilot™" />
+</Section>
