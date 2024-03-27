e2c9d9c8341aaa9b9814252e2626584ded5c43a3
Nguyen Minh Tuan <tnowad@gmail.com>
2024-03-28 00:38:16 +0700

    feat: add type for article and category

--- news-copilot-frontend/src/lib/services/types/index.d.ts
+++ news-copilot-frontend/src/lib/services/types/index.d.ts
@@ -7,3 +7,24 @@ export type User = {
 	avatarImage: string;
 	roles?: Role[];
 };
+
+export type Article = {
+	id: number;
+	title: string;
+	slug: string;
+	summary: string;
+	coverImage: string;
+	author?: User;
+	content?: string;
+	createdAt?: string;
+	updatedAt?: string;
+	publishedAt?: string;
+};
+
+export type Category = {
+	id: number;
+	title: string;
+	slug: string;
+	description?: string;
+	articles?: Article[];
+};
