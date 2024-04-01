a6719f5b7978fb0c56d1eaf88409f15389538f9f
Nguyen Minh Tuan <tnowad@gmail.com>
2024-04-01 19:54:28 +0700

    feat: improve get all in comments endpoint

--- news-copilot-backend/app/routes/comments.py
+++ news-copilot-backend/app/routes/comments.py
@@ -1,40 +1,135 @@
-from flask import Blueprint, jsonify, request
-
-from app.models.comment import Comment
-from app.extensions import db
 from http import HTTPStatus
+
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
+
+from app.extensions import db
+from app.models.article import Article
+from app.models.comment import Comment
+from app.models.user import User
 
 comments_bp = Blueprint("comments", __name__)
 
 
 @comments_bp.route("/comments", methods=["GET"])
 def get_comments():
-    comments = Comment.query.all()
+    try:
+        page = request.args.get("page", type=int) or 1
+        limit = request.args.get("limit", type=int) or 10
+        sort_by = request.args.get("sortBy", type=str)
+        sort_order = request.args.get("sortOrder", type=str)
+        style = request.args.get("style", type=str)
+        includes = request.args.getlist("includes", type=str)
+        user_id = request.args.get("userId", type=int)
+        article_id = request.args.get("articleId", type=int)
+        search = request.args.get("search", type=str)
 
-    response_data = {
-        "statusCode": HTTPStatus.OK,
-        "message": "Get all comments successful",
-        "data": {
-            "comments": [
-                {
-                    "id": comment.id,
-                    "content": comment.content,
-                    "created_at": comment.created_at,
-                    "updated_at": comment.updated_at,
-                    "author": {
-                        "id": comment.author.id,
-                        "email": comment.author.email,
-                        "display_name": comment.author.display_name,
-                        "avatar_image": comment.author.avatar_image,
-                    },
+        query = Comment.query
+        query = query.filter(Comment.deleted_at.is_(None))
+
+        if user_id:
+            query = query.filter(Comment.author_id == user_id)  # type: ignore
+
+        if article_id:
+            query = query.filter(Comment.article_id == article_id)  # type: ignore
+
+        if search:
+            query = query.filter(Comment.content.ilike(f"%{search}%"))  # type: ignore
+
+        if sort_by and sort_order:
+            if sort_by == "user":
+                query = query.join(User).order_by(
+                    User.display_name.asc()  # type: ignore
+                    if sort_order == "asc"
+                    else User.display_name.desc()  # type: ignore
+                )
+            elif sort_by == "article":
+                query = query.join(Article).order_by(
+                    Article.title.asc()  # type: ignore
+                    if sort_order == "asc"
+                    else Article.title.desc()  # type: ignore
+                )
+            elif sort_by == "createdAt":
+                query = query.order_by(
+                    Comment.created_at.asc()
+                    if sort_order == "asc"
+                    else Comment.created_at.desc()
+                )
+
+        if page and limit:
+            offset = (page - 1) * limit
+            query = query.offset(offset).limit(limit)
+
+        comments = query.all()
+
+        comments_data = []
+        for comment in comments:
+            comment_info = {
+                "id": comment.id,
+                "content": comment.content,
+                "created_at": comment.created_at,
+                "updated_at": comment.updated_at,
+            }
+
+            if style == "full":
+                comment_info["deleted_at"] = comment.deleted_at
+
+            if "author" in includes and comment.author:
+                comment_info["author"] = {
+                    "id": comment.author.id,
+                    "email": comment.author.email,
+                    "displayName": comment.author.display_name,
+                    "avatarImage": comment.author.avatar_image,
                 }
-                for comment in comments
-            ]
-        },
-    }
 
-    return jsonify(response_data), HTTPStatus.OK
+            if "article" in includes and comment.article:
+                comment_info["article"] = {
+                    "id": comment.article.id,
+                    "title": comment.article.title,
+                    "summary": comment.article.summary,
+                    "createdAt": comment.article.created_at,
+                    "coverImage": comment.article.cover_image,
+                }
+
+            comments_data.append(comment_info)
+
+        metadata = {
+            "pagination": {
+                "offset": (page - 1) * limit if (page and limit) else None,
+                "limit": limit,
+                "previousOffset": (page - 2) * limit if page > 1 else None,
+                "nextOffset": page * limit if comments else None,
+                "currentPage": page if page else None,
+                "totalCount": query.count(),
+            },
+            "sortedBy": {
+                "name": sort_by,
+                "order": sort_order,
+            },
+        }
+
+        response_data = {
+            "statusCode": HTTPStatus.OK,
+            "message": "Get all comments route",
+            "data": {
+                "comments": comments_data,
+                "metadata": metadata,
+            },
+        }
+
+        return jsonify(response_data), HTTPStatus.OK
+
+    except Exception as e:
+        return (
+            jsonify(
+                {
+                    "statusCode": HTTPStatus.INTERNAL_SERVER_ERROR,
+                    "message": "Internal Server Error",
+                    "error": str(e),
+                }
+            ),
+            HTTPStatus.INTERNAL_SERVER_ERROR,
+        )
 
 
 @comments_bp.route("/comments", methods=["POST"])
