44626b3420b65290fa5eaaf2e750ef9e84163285
EpikPotato <duccute3@gmail.com>
2024-04-03 16:46:35 +0700

    feat: adding create comment feature

--- news-copilot-frontend/src/lib/services/comment.service.ts
+++ news-copilot-frontend/src/lib/services/comment.service.ts
@@ -3,8 +3,8 @@ import { defaultHeaders } from './config';
 import { API_URL } from '$env/static/private';
 type CreateCommentBody = {
 	content: string
-	author_id: number
-	article_id: number
+	authorId: number
+	articleId: number
 }
 type CreateCommentSuccessful = {
 	statuscode: StatusCodes.CREATED;
@@ -60,26 +60,103 @@ const createComment = async (body: CreateCommentBody, headers: HeadersInit = {})
 	const response = (await fetch(url, requestInit)) as CreateCommentResponse
 	return response.json()
 }
+
+
+
 type GetAllCommentSuccessful = {
-	statusCode: StatusCodes.ACCEPTED,
+	statusCode: StatusCodes.OK;
 	data: {
-		id: number,
-		content: string,
-		created_at: string,
-		updated_at: string,
-		author: {
-			id: number,
-			display_name: string,
-			display_image: string
+		comments: {
+			id: number;
+			content: string;
+			createdAt: string;
+			updatedAt: string;
+			articleID : number;
+			author: {
+				id: number;
+				displayName: string;
+				avatarImage: string;
+			};
+		}[];
+		metadata: {
+			pagination: {
+				offset: number;
+				limit: number;
+				previousOffset: number;
+				nextOffset: number;
+				currentPage: number;
+				totalCount: number;
+			};
+			sortedBy: {
+				name: 'date' | 'name';
+				sortOrder: 'asc' | 'desc';
+			};
+			filter: {
+				articleId: string;
+				userId: string;
+				parentId: string;
+				search: string;
+			};
 		};
-	}[];
+		message: string;
+	}
 }
+type GetAllCommentsServerError = {
+	statusCode: StatusCodes.INTERNAL_SERVER_ERROR;
+	message: string;
+	error: string;
+}
+type GetAllCommentsParams = {
+	page?: number;
+	search?: string;
+	articleId?: number;
+	limit?: number;
+	sortBy?: 'name' | 'title';
+	sortOrder?: 'asc' | 'desc';
+	style?: 'full',
+	includes?: 'author'[]
 
+}
+type GetAllCommentsRespone = Omit<Response, 'json'> & {
+	json: () => Promise<GetAllCommentSuccessful | GetAllCommentsServerError>;
+}
+const getAllComments = async (params: GetAllCommentsParams = {}, headers: HeadersInit = {}) => {
+	try {
+		const { page, limit, search, sortBy, sortOrder, style, includes , articleId: articleID } = params;
+		const url = new URL('/comments', API_URL);
+		const queryParams = new URLSearchParams();
+		if (page) queryParams.set('page', page.toString());
+		if (limit) queryParams.set('limit', limit.toString());
+		if (search) queryParams.set('search', search);
+		if (sortBy) queryParams.set('sortBy', sortBy);
+		if (sortOrder) queryParams.set('sortOrder', sortOrder);
+		if (style) queryParams.set('style', style);
+		if (articleID) queryParams.set('articleID', articleID.toString());
+		if (includes) includes.forEach((param) => queryParams.append('includes', param));
 
+		url.search = queryParams.toString();
 
+		const requestInit: RequestInit = {
+			method: 'GET',
+			headers: { ...defaultHeaders, ...headers }
+		};
 
+		const response = (await fetch(url, requestInit)) as GetAllCommentsRespone;
+
+		return response.json();
+	} catch (error) {
+		throw new Error('Failed to fetch articles: ' + (error as Error).message);
+	}
+}
+type GetCommentByArticleIDServerError = {
+	statusCode : StatusCodes.INTERNAL_SERVER_ERROR;
+	message : string;
+	error:string;
+	
+}
 
 const commentServerices = {
+	getAllComments,
 	createComment
 }
-export default createComment
+export default commentServerices;
