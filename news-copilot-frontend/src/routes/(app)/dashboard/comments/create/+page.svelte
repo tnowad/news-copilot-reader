49052009498a97996242dc8d7077ee8ecd67b7da
QTLitSD <117745574+QTLitSD@users.noreply.github.com>
2024-04-03 16:12:03 +0700

    feat: get user data

---
+++ news-copilot-frontend/src/routes/(app)/dashboard/comments/create/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { Card } from 'flowbite-svelte';
+	import Markdown from '$lib/widgets/markdown.svelte';
+
+	let content = '';
+
+	const handleSubmit = () => {};
+
+
+</script>
+
+
+<section class="mx-5">
+	<h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Comment Article</h2>
+	<form on:submit={handleSubmit}>
+		<div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+
+        <Card href="/articles" class="mx-auto">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </Card>
+
+        </div>
+        </form>
+
+<div>
+    <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Content Preview</h2>
+    <div>
+        <Markdown bind:source={content} />
+    </div>
+</div>
+</section>
