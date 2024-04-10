882304eb0deece219011fbce8739e45c8e32e407
QTLitSD <117745574+QTLitSD@users.noreply.github.com>
2024-04-10 15:09:32 +0700

    fix: category card

--- news-copilot-frontend/src/routes/(app)/(default)/categories/[slug]/[id]/+page.svelte
+++ news-copilot-frontend/src/routes/(app)/(default)/categories/[slug]/[id]/+page.svelte
@@ -10,7 +10,7 @@
 
 <div>
 	{#if data.category}
-		<Card>
+		<Card size="none">
 			<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
 				{data.category.title}
 			</h5>
@@ -19,9 +19,6 @@
 					{data.category.description}
 				</p>
 			{/if}
-			<Button class="w-fit">
-				Read more <ArrowRightOutline class="ms-2 h-3.5 w-3.5 text-white" />
-			</Button>
 		</Card>
 	{:else}
 		<p>Không có dữ liệu.</p>
