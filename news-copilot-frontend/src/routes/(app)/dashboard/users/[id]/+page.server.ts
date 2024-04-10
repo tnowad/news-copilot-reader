52819774e0a44ab83405edbd2d0352e3bb1ee934
Nguyen Minh Tuan <tnowad@gmail.com>
2024-04-10 17:33:16 +0700

    feat: add update user id

---
+++ news-copilot-frontend/src/routes/(app)/dashboard/users/[id]/+page.server.ts
@@ -0,0 +1,67 @@
+import userService from '$lib/services/user.service';
+import { StatusCodes } from 'http-status-codes';
+import type { Actions, PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+
+export const load: PageServerLoad = async (event) => {
+	const accessToken = event.cookies.get('accessToken');
+
+	if (!accessToken) {
+		redirect(StatusCodes.TEMPORARY_REDIRECT, '/sign-in');
+	}
+
+	// Change to get user by id
+	const currentUserProfileResponse = await userService.getCurrentUserProfile(
+		{ include: ['roles'], style: 'full' },
+		{ Authorization: `Bearer ${accessToken}` }
+	);
+
+	const user =
+		currentUserProfileResponse.statusCode === StatusCodes.OK
+			? currentUserProfileResponse.data.user
+			: null;
+
+	return { user: user };
+};
+
+export const actions = {
+	default: async (event) => {
+		const formData = await event.request.formData();
+		const email = formData.get('email') as string;
+		const birthDate = formData.get('birthDate') as string;
+		const phoneNumber = formData.get('phoneNumber') as string;
+		const password = formData.get('password') as string;
+		const newPassword = formData.get('newPassword') as string;
+		const displayName = formData.get('displayName') as string;
+		const avatarImage = formData.get('avatarImage') as string;
+		const bio = formData.get('bio') as string;
+
+		// Change to update user id
+		const response = await userService.updateCurrentUser(
+			{
+				email,
+				avatarImage,
+				displayName,
+				bio,
+				phoneNumber,
+				birthDate,
+				newPassword,
+				password
+			},
+			{ Authorization: `Bearer ${event.cookies.get('accessToken')}` }
+		);
+
+		switch (response.statusCode) {
+			case StatusCodes.OK:
+				break;
+			case StatusCodes.NOT_FOUND:
+			case StatusCodes.FORBIDDEN:
+				break;
+			case StatusCodes.UNPROCESSABLE_ENTITY:
+				break;
+			default:
+		}
+
+		return response;
+	}
+} satisfies Actions;
---
+++ news-copilot-frontend/src/routes/(app)/dashboard/users/[id]/+page.svelte
@@ -0,0 +1,157 @@
+<script lang="ts">
+	import {
+		Card,
+		Heading,
+		Breadcrumb,
+		BreadcrumbItem,
+		Avatar,
+		Button,
+		Input,
+		Label,
+		Badge,
+		Textarea
+	} from 'flowbite-svelte';
+	import { UploadSolid } from 'flowbite-svelte-icons';
+	import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
+	import { onMount } from 'svelte';
+
+	export let data: PageData;
+
+	let avatarInputElement: HTMLInputElement | null = null;
+	let avatarImageSrc = data.user?.avatarImage ?? '/images/default-profile-picture.png';
+
+	onMount(() => {
+		if (avatarInputElement) {
+			avatarInputElement.onchange = () => {
+				if (avatarInputElement == null) {
+					return;
+				}
+
+				if (!avatarInputElement.files?.length) {
+					return;
+				}
+
+				if (avatarInputElement.files.length > 0) {
+					const file = avatarInputElement.files[0];
+					avatarImageSrc = URL.createObjectURL(file);
+				}
+			};
+		}
+	});
+</script>
+
+<section>
+	<div class="">
+		<div class="col-span-full mt-6 xl:mb-0">
+			<Breadcrumb class="mb-6">
+				<BreadcrumbItem home>Home</BreadcrumbItem>
+				<BreadcrumbItem
+					class="inline-flex items-center text-gray-700 hover:text-primary-600 dark:text-gray-300 dark:hover:text-white"
+					href="/curd/users">Users</BreadcrumbItem
+				>
+				<BreadcrumbItem>Settings</BreadcrumbItem>
+			</Breadcrumb>
+
+			<Heading tag="h1" class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+				User settings
+			</Heading>
+		</div>
+		<Card class="xl:col-span-8" size="none">
+			<Heading tag="h3" class="text-xl">General Information</Heading>
+			<form
+				class="grid grid-cols-6 gap-6"
+				action="/profile"
+				method="post"
+				use:enhance
+				enctype="multipart/form-data"
+			>
+				<Label class="col-span-6 space-y-2 sm:col-span-3">
+					<span>Display Name:</span>
+					<Input
+						type="text"
+						class="border font-normal outline-none"
+						name="displayName"
+						value={data.user?.displayName}
+					/>
+				</Label>
+
+				<Label class="col-span-6 space-y-2 sm:col-span-3">
+					<span>Phone number:</span>
+					<Input
+						type="text"
+						value={data.user?.phoneNumber}
+						name="phoneNumber"
+						class="border font-normal outline-none"
+					/>
+				</Label>
+
+				<Label class="col-span-6 space-y-2 sm:col-span-3">
+					<span>Email address:</span>
+					<Input
+						type="text"
+						class="border font-normal outline-none"
+						value={data.user?.email}
+						name="email"
+						disabled
+					/>
+				</Label>
+
+				<Label class="col-span-6 space-y-2 sm:col-span-3">
+					<span>Birthday:</span>
+					<Input
+						type="date"
+						value={data.user?.birthDate}
+						name="birthDate"
+						class="border font-normal outline-none"
+					/>
+				</Label>
+
+				<Label class="col-span-6 space-y-2 sm:col-span-3">
+					<span>Password:</span>
+					<Input
+						type="password"
+						placeholder="••••••••"
+						name="password"
+						class="border font-normal outline-none"
+						required
+					/>
+				</Label>
+
+				<Label class="col-span-6 space-y-2 sm:col-span-3">
+					<span>New Password:</span>
+					<Input
+						type="password"
+						name="newPassword"
+						placeholder="••••••••"
+						class="border font-normal outline-none"
+					/>
+				</Label>
+
+				<Label class="col-span-full space-y-2">
+					<span>Bio:</span>
+					<Textarea
+						value={data.user?.bio}
+						name="bio"
+						class="w-full border font-normal outline-none"
+					/>
+				</Label>
+
+				<Label class="hidden">
+					<span>Avatar Image</span>
+					<input bind:this={avatarInputElement} type="file" name="avatarImage" />
+				</Label>
+				<Label class="col-span-full space-y-2">
+					<span>Role:</span>
+					<div class="flex gap-x-2">
+						{#each data.user?.roles ?? [] as role}
+							<Badge>{role}</Badge>
+						{/each}
+					</div>
+				</Label>
+
+				<Button type="submit" class="w-fit whitespace-nowrap">Save all</Button>
+			</form>
+		</Card>
+	</div>
+</section>
