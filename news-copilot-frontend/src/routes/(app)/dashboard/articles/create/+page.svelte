8aebffb1d1751d81f3c7cd032e51c9cb437bf295
Nguyen Minh Tuan <tnowad@gmail.com>
2024-04-15 08:26:25 +0700

    refactor: remove unused variable

--- news-copilot-frontend/src/routes/(app)/dashboard/articles/create/+page.svelte
+++ news-copilot-frontend/src/routes/(app)/dashboard/articles/create/+page.svelte
@@ -2,19 +2,19 @@
 	import { Label, Input, Button, MultiSelect, Textarea } from 'flowbite-svelte';
 	import Markdown from '$lib/widgets/markdown.svelte';
 	import Editor from '$lib/widgets/editor.svelte';
-	import type { PageData } from './$types';
-	let content = '';
-	let coverImage = '';
-	let summary = '';
-	const handleSubmit = () => {};
 
-	let selected: { value: string; name: string }[] = [];
-	export let data: PageData;
+	export let data;
+
+	let content = '';
+	let categories = data.categories.map((category) => ({
+		value: category.id,
+		name: category.slug
+	}));
 </script>
 
 <section class="mx-5">
-	<h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new Article</h2>
-	<form action="/dashboard/articles/create" method="post">
+	<h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Update Article</h2>
+	<form action={`/dashboard/articles/create`} method="post">
 		<div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
 			<div class="sm:col-span-2">
 				<Label for="name" class="mb-2">Title</Label>
@@ -22,12 +22,7 @@
 			</div>
 			<div class="sm:col-span-2">
 				<Label for="cover-image" class="mb-2">Cover Image</Label>
-				<Input
-					type="text"
-					name="coverImage"
-					placeholder="URL of the cover image"
-					bind:value={coverImage}
-				/>
+				<Input type="text" name="coverImage" placeholder="URL of the cover image" />
 			</div>
 			<div class="sm:col-span-2">
 				<Label for="author-id" class="mb-2">Author ID</Label>
@@ -42,28 +37,15 @@
 			</div>
 			<div class="col-span-full">
 				<Label for="categories" class="mb-2">Categories</Label>
-				<MultiSelect
-					name="category"
-					items={data.categories.map((category) => ({
-						value: category.id,
-						name: category.slug
-					}))}
-					bind:selected
-				/>
+				<MultiSelect name="category" items={categories} />
 			</div>
 			<div class="sm:col-span-2">
 				<Label for="summary" class="mb-2">Summary</Label>
-				<Textarea
-					type="text"
-					name="summary"
-					id="summary"
-					placeholder="Summary"
-					bind:value={summary}
-				/>
+				<Textarea type="text" name="summary" id="summary" placeholder="Summary" />
 			</div>
 			<div class="sm:col-span-2">
 				<Label for="description" class="mb-2">Content</Label>
-				<Editor bind:source={content} />
+				<Editor bind:content />
 				<input type="hidden" name="content" bind:value={content} />
 			</div>
 			<Button type="submit" class="w-32">Add product</Button>
