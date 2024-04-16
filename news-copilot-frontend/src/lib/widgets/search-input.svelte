d2fab7f33236fbd0feee013116cad9d13fd1d582
Nguyen Minh Tuan <tnowad@gmail.com>
2024-04-16 16:51:39 +0700

    chore: remove unused define

--- news-copilot-frontend/src/lib/widgets/search-input.svelte
+++ news-copilot-frontend/src/lib/widgets/search-input.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { Input, Button, Modal, Search, Listgroup, ListgroupItem, Card } from 'flowbite-svelte';
+	import { Input, Button, Modal, Search } from 'flowbite-svelte';
 	import { ArrowRightOutline, SearchOutline } from 'flowbite-svelte-icons';
 
 	let query = '';
@@ -17,37 +16,6 @@
 		coverImage: string;
 		slug: string;
 	}[] = [];
-
-	const mockArticles = [
-		{
-			id: 1,
-			title: 'How to build a website using SvelteKit',
-			summary: 'Learn how to build a website using SvelteKit and deploy it to Vercel.',
-			coverImage: '/images/default-news-cover.png',
-			slug: 'how-to-build-a-website-using-sveltekit'
-		},
-		{
-			id: 2,
-			title: 'How to build a website using SvelteKit',
-			summary: 'Learn how to build a website using SvelteKit and deploy it to Vercel.',
-			coverImage: '/images/default-news-cover.png',
-			slug: 'how-to-build-a-website-using-sveltekit'
-		},
-		{
-			id: 3,
-			title: 'How to build a website using SvelteKit',
-			summary: 'Learn how to build a website using SvelteKit and deploy it to Vercel.',
-			coverImage: '/images/default-news-cover.png',
-			slug: 'how-to-build-a-website-using-sveltekit'
-		},
-		{
-			id: 4,
-			title: 'How to build a website using SvelteKit',
-			summary: 'Learn how to build a website using SvelteKit and deploy it to Vercel.',
-			coverImage: '/images/default-news-cover.png',
-			slug: 'how-to-build-a-website-using-sveltekit'
-		}
-	];
 
 	const getAllArticles = async (query: string) => {
 		const response = await fetch(`/api/articles?search=${query}`, {
@@ -106,7 +74,11 @@
 			on:click={toggleSearchModal}
 		>
 			<div class="h-20 w-20">
-				<img src={article.coverImage} alt={article.title} class="h-full w-full object-cover" />
+				<img
+					src={article.coverImage ?? '/images/logo.png'}
+					alt={article.title}
+					class="h-full w-full object-cover"
+				/>
 			</div>
 			<div>
 				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
