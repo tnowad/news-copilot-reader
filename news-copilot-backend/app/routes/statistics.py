107af871a35a63e631ad8da902c66ac94be3f8f8
Nguyen Minh Tuan <tnowad@gmail.com>
2024-04-17 13:23:51 +0700

    feat: add statics article by category

--- news-copilot-backend/app/routes/statistics.py
+++ news-copilot-backend/app/routes/statistics.py
@@ -1,9 +1,13 @@
 from http import HTTPStatus
 from flask import Blueprint, jsonify
+from sqlalchemy import func
 
 from app.models.article import Article
+from app.models.category import Category
 from app.models.view import View
 from app.models.comment import Comment
+
+from app.extensions import db
 
 statistics_bp = Blueprint("statistics", __name__)
 
@@ -25,5 +29,36 @@ def articles_statistics():
             "statusCode": HTTPStatus.OK,
             "data": statistics,
             "message": "Statistics retrieved successfully",
+        }
+    )
+
+
+@statistics_bp.route("/statistics/categories-articles", methods=["GET"])
+def category_article_count():
+    category_counts = (
+        db.session.query(Category.title, func.count(Article.id))
+        .outerjoin(Article.categories)
+        .group_by(Category.title)
+        .all()
+    )
+
+    categories = []
+    counts = []
+    for category, count in category_counts:
+        categories.append(category)
+        counts.append(count)
+
+    apex_options = {
+        "chart": {"type": "bar"},
+        "series": [{"name": "Articles", "data": counts}],
+        "xaxis": {"categories": categories},
+        "title": {"text": "Articles per Category"},
+    }
+
+    return jsonify(
+        {
+            "statusCode": HTTPStatus.OK,
+            "data": apex_options,
+            "message": "Category article counts retrieved successfully",
         }
     )
