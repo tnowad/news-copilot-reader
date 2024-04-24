ab027f55250dc43ef658cd4f4cb4b97ea5206222
EpikPotato <duccute3@gmail.com>
2024-04-24 15:25:04 +0700

    feat: adding statistuis

--- news-copilot-frontend/src/routes/(app)/dashboard/+page.svelte
+++ news-copilot-frontend/src/routes/(app)/dashboard/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Chart, Card } from 'flowbite-svelte';
+	import { Section, Social } from 'flowbite-svelte-blocks';
 	export let data;
 	$: console.log(data);
 </script>
@@ -11,3 +12,26 @@
 		</Card>
 	</div>
 </section>
+<br />
+<Card class="col-span-6" size="none">
+	<Social>
+		<div class="flex flex-col items-center justify-center">
+			<dt class="mb-2 text-3xl font-extrabold md:text-4xl">
+				{data.articlesStatistics?.totalArticles}
+			</dt>
+			<dd class="font-light text-gray-500 dark:text-gray-400">Articles</dd>
+		</div>
+		<div class="flex flex-col items-center justify-center">
+			<dt class="mb-2 text-3xl font-extrabold md:text-4xl">
+				{data.articlesStatistics?.totalUser}
+			</dt>
+			<dd class="font-light text-gray-500 dark:text-gray-400">Users</dd>
+		</div>
+		<div class="flex flex-col items-center justify-center">
+			<dt class="mb-2 text-3xl font-extrabold md:text-4xl">
+				{data.articlesStatistics?.totalCategories}
+			</dt>
+			<dd class="font-light text-gray-500 dark:text-gray-400">Categories</dd>
+		</div>
+	</Social>
+</Card>
